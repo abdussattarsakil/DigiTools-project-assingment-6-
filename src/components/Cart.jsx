a241310ea106b16c2d0f2618f4661b10817963e3
abdussattarsakil <abdussattarsakil208@gmail.com>
2026-04-29 14:04:09 +0600

    add to cart show done

--- src/components/Cart.jsx
+++ src/components/Cart.jsx
@@ -4,15 +4,27 @@ const Cart = ({ carts }) => {
             <div className="card container bg-base-100 shadow-sm m-auto">
                 <div className="card-body">
                     <h2>Your Cart</h2>
-                    <div className="space-y-3">
+                    <div className="space-y-3 ">
                         {
                             carts.map(cart => (
-                                <div className="card bg-base-200 shadow-sm flex-row">
-                                    <img className="w-1/12" src={cart.icon} alt="" />
-                                    <div className="m-auto">
-                                        <h1>{cart.name}</h1>
+                                <div className="card bg-base-200 shadow-sm p-3">
+                                    <div className="flex justify-between items-center">
+
+                                        {/* Left Side */}
+                                        <div className="flex items-center gap-3 ">
+                                            <img className="w-10" src={cart.icon} alt="" />
+
+                                            <div>
+                                                <h1 className="font-semibold">{cart.name}</h1>
+                                                <p className="text-sm text-gray-500">${cart.price}</p>
+                                            </div>
+                                        </div>
+
+                                        {/* Right Side */}
+                                        <div>
+                                            <p className="text-red-500 cursor-pointer border border-gray-400 rounded-full p-1">Remove</p>
+                                        </div>
                                     </div>
-                                    
                                 </div>
                             ))
                         }
