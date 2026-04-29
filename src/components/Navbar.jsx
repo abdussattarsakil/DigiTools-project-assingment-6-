1ebf67e2bde61f8da35180b4b16c83f698d7f0b4
abdussattarsakil <abdussattarsakil208@gmail.com>
2026-04-29 21:23:23 +0600

    navbar responsive done

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -32,7 +32,7 @@ const Navbar = ({carts}) => {
                         <svg xmlns="http://www.w3.org/2000/svg" className="h-5 w-5" fill="none" viewBox="0 0 24 24" stroke="currentColor"> <path strokeLinecap="round" strokeLinejoin="round" strokeWidth="2" d="M3 3h2l.4 2M7 13h10l4-8H5.4M7 13L5.4 5M7 13l-2.293 2.293c-.63.63-.184 1.707.707 1.707H17m0 0a2 2 0 100 4 2 2 0 000-4zm-8 2a2 2 0 11-4 0 2 2 0 014 0z" /> </svg>
                         <span className="badge badge-sm indicator-item ">{carts.length}</span>
                     </div>
-                    <a>Login</a>
+                    <a className="hidden sm:block">Login</a>
                     <h2 className="btn rounded-full bg-linear-to-r/hsl from-blue-500 to-purple-500 text-white">Get Started</h2>
                 </div>
             </div>
